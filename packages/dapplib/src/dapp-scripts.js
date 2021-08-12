43a5bf0aba590614eb4c57727872f71d008a65f0
Jonathan Sheely <jsheely@thirtytech.net>
2021-08-12 12:54:13 -0400

    Downloader

--- packages/dapplib/src/dapp-scripts.js
+++ packages/dapplib/src/dapp-scripts.js
@@ -43,80 +43,6 @@ module.exports = class DappScripts {
 		`;
 	}
 
-	static kittyitems_read_collection_ids() {
-		return fcl.script`
-				import NonFungibleToken from 0x01cf0e2f2f715450
-				import KittyItems from 0x01cf0e2f2f715450
-				
-				// This script returns an array of all the NFT IDs in an account's collection.
-				
-				pub fun main(address: Address): [UInt64] {
-				    let account = getAccount(address)
-				
-				    let collectionRef = account.getCapability(KittyItems.CollectionPublicPath)!.borrow<&{NonFungibleToken.CollectionPublic}>()
-				        ?? panic("Could not borrow capability from public collection")
-				    
-				    return collectionRef.getIDs()
-				}
-		`;
-	}
-
-	static kittyitems_read_kitty_item_type_id() {
-		return fcl.script`
-				import NonFungibleToken from 0x01cf0e2f2f715450
-				import KittyItems from 0x01cf0e2f2f715450
-				
-				// This script returns the metadata for an NFT in an account's collection.
-				
-				pub fun main(address: Address, itemID: UInt64): UInt64 {
-				
-				    // get the public account object for the token owner
-				    let owner = getAccount(address)
-				
-				    let collectionBorrow = owner.getCapability(KittyItems.CollectionPublicPath)!
-				        .borrow<&{KittyItems.KittyItemsCollectionPublic}>()
-				        ?? panic("Could not borrow KittyItemsCollectionPublic")
-				
-				    // borrow a reference to a specific NFT in the collection
-				    let kittyItem = collectionBorrow.borrowKittyItem(id: itemID)
-				        ?? panic("No such itemID in that collection")
-				
-				    return kittyItem.typeID
-				}
-		`;
-	}
-
-	static kittyitems_read_collection_length() {
-		return fcl.script`
-				import NonFungibleToken from 0x01cf0e2f2f715450
-				import KittyItems from 0x01cf0e2f2f715450
-				
-				// This script returns the size of an account's KittyItems collection.
-				
-				pub fun main(address: Address): Int {
-				    let account = getAccount(address)
-				
-				    let collectionRef = account.getCapability(KittyItems.CollectionPublicPath)!
-				        .borrow<&{NonFungibleToken.CollectionPublic}>()
-				        ?? panic("Could not borrow capability from public collection")
-				    
-				    return collectionRef.getIDs().length
-				}
-		`;
-	}
-
-	static kittyitems_read_kitty_items_supply() {
-		return fcl.script`
-				import KittyItems from 0x01cf0e2f2f715450
-				
-				// This scripts returns the number of KittyItems currently in existence.
-				
-				pub fun main(): UInt64 {    
-				    return KittyItems.totalSupply
-				}
-		`;
-	}
-
 	static kittyitemsmarket_read_collection_ids() {
 		return fcl.script`
 				import KittyItemsMarket from 0x01cf0e2f2f715450
@@ -152,6 +78,80 @@ module.exports = class DappScripts {
 				        ?? panic("Could not borrow market collection from market address")
 				    
 				    return marketCollectionRef.getSaleOfferIDs().length
+				}
+		`;
+	}
+
+	static kittyitems_read_collection_ids() {
+		return fcl.script`
+				import NonFungibleToken from 0x01cf0e2f2f715450
+				import KittyItems from 0x01cf0e2f2f715450
+				
+				// This script returns an array of all the NFT IDs in an account's collection.
+				
+				pub fun main(address: Address): [UInt64] {
+				    let account = getAccount(address)
+				
+				    let collectionRef = account.getCapability(KittyItems.CollectionPublicPath)!.borrow<&{NonFungibleToken.CollectionPublic}>()
+				        ?? panic("Could not borrow capability from public collection")
+				    
+				    return collectionRef.getIDs()
+				}
+		`;
+	}
+
+	static kittyitems_read_collection_length() {
+		return fcl.script`
+				import NonFungibleToken from 0x01cf0e2f2f715450
+				import KittyItems from 0x01cf0e2f2f715450
+				
+				// This script returns the size of an account's KittyItems collection.
+				
+				pub fun main(address: Address): Int {
+				    let account = getAccount(address)
+				
+				    let collectionRef = account.getCapability(KittyItems.CollectionPublicPath)!
+				        .borrow<&{NonFungibleToken.CollectionPublic}>()
+				        ?? panic("Could not borrow capability from public collection")
+				    
+				    return collectionRef.getIDs().length
+				}
+		`;
+	}
+
+	static kittyitems_read_kitty_item_type_id() {
+		return fcl.script`
+				import NonFungibleToken from 0x01cf0e2f2f715450
+				import KittyItems from 0x01cf0e2f2f715450
+				
+				// This script returns the metadata for an NFT in an account's collection.
+				
+				pub fun main(address: Address, itemID: UInt64): UInt64 {
+				
+				    // get the public account object for the token owner
+				    let owner = getAccount(address)
+				
+				    let collectionBorrow = owner.getCapability(KittyItems.CollectionPublicPath)!
+				        .borrow<&{KittyItems.KittyItemsCollectionPublic}>()
+				        ?? panic("Could not borrow KittyItemsCollectionPublic")
+				
+				    // borrow a reference to a specific NFT in the collection
+				    let kittyItem = collectionBorrow.borrowKittyItem(id: itemID)
+				        ?? panic("No such itemID in that collection")
+				
+				    return kittyItem.typeID
+				}
+		`;
+	}
+
+	static kittyitems_read_kitty_items_supply() {
+		return fcl.script`
+				import KittyItems from 0x01cf0e2f2f715450
+				
+				// This scripts returns the number of KittyItems currently in existence.
+				
+				pub fun main(): UInt64 {    
+				    return KittyItems.totalSupply
 				}
 		`;
 	}
